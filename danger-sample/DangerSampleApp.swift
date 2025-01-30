3e40bbff8e227a1afb601a3db4122876fe214842
taise-hikawa <cmmkarubi@gmail.com>
2025-01-30 11:07:40 +0900

    add xcode proj

---
+++ danger-sample/DangerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  danger_sampleApp.swift
+//  danger-sample
+//
+//  Created by 樋川大聖 on 2025/01/30.
+//
+
+import SwiftUI
+
+@main
+struct DangerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

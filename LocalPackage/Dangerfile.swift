60e306d5949965e1a1b7eb60ba926461529c927e
taise-hikawa <cmmkarubi@gmail.com>
2025-01-30 20:54:51 +0900

    add danger

--- LocalPackage/Dangerfile.swift
+++ LocalPackage/Dangerfile.swift
@@ -8,14 +8,14 @@
 import Danger
 let danger = Danger()
 
-let github = danger.github
+let github = danger.github!
 let pullRequest = github.pullRequest
 
 let linesAdded = pullRequest.additions ?? 0
 let linesDeleted = pullRequest.deletions ?? 0
 let filesChanged = pullRequest.changedFiles ?? 0
 
-var bigPRThreshold = 500;
+let bigPRThreshold = 1
 if linesAdded + linesDeleted > bigPRThreshold || filesChanged > 15 {
     warn("Pull Request size seems large. If this Pull Request contains multiple changes, please split each into separate PRs, this will enable faster & easier review.");
 }
--- LocalPackage/Package.swift
+++ LocalPackage/Package.swift
@@ -6,15 +6,22 @@ import PackageDescription
 let package = Package(
     name: "LocalPackage",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+        // Building/Testing for iOS is not yet supported by the SPM.
+        .macOS(.v10_15)
     ],
     products: [
-        .library(
-            name: "Hoge",
-            targets: ["Hoge"]
-        )
+        .library(name: "Hoge", targets: ["Hoge"]),
+        .library(name: "DangerDeps", type: .dynamic, targets: ["DangerTarget"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/danger/swift.git", .upToNextMajor(from: "3.0.0"))
     ],
     targets: [
-        .target(name: "Hoge")
+        .target(name: "Hoge"),
+        .target(
+            name: "DangerTarget",
+            dependencies: [.product(name: "Danger", package: "swift")]
+        )
     ]
 )

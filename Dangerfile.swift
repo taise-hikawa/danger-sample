cf161c1217392b659b3b5e7ed9a54c05bd5e6dca
taise-hikawa <cmmkarubi@gmail.com>
2025-02-19 11:28:40 +0900

    test

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -9,13 +9,12 @@ import Danger
 import Foundation
 
 private let danger = Danger()
-private let prChecker = PRChecker(danger: danger)
-let pullRequest: danger.github.pullRequest
-let linesAdded = pullRequest.additions ?? 0
-let linesDeleted = pullRequest.deletions ?? 0
-let filesChanged = pullRequest.changedFiles ?? 0
-let bigPRThreshold = 1 // test
-let fileChangesThreshold = 15
+private let pullRequest: danger.github.pullRequest
+private let linesAdded = pullRequest.additions ?? 0
+private let linesDeleted = pullRequest.deletions ?? 0
+private let filesChanged = pullRequest.changedFiles ?? 0
+private let bigPRThreshold = 1 // test
+private let fileChangesThreshold = 15
 
 if linesAdded + linesDeleted > bigPRThreshold || filesChanged > fileChangesThreshold {
     warn("""
